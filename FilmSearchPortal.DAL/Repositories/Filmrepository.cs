ea4ec75f1bb4707b3294dfde2ead1a23b8a12c42
spiraspira <zabornik5350@gmail.com>
2024-01-25 13:54:40 +0300

    Added FilmRepository

---
+++ FilmSearchPortal.DAL/Repositories/Filmrepository.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmSearchPortal.DAL.Repositories;
+
+public class FilmRepository(ApplicationDbContext context) : GenericRepository<Film>(context), IFilmRepository
+{
+	public async Task<IEnumerable<Film>> GetFilmsByTitle(string query)
+	{
+		return await Set
+			.Where(film => film.Title != null && film.Title.Contains(query))
+			.ToListAsync();
+	}
+}
