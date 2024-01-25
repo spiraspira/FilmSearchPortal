e111693c3a95902b46d272dd76883ba169bb8f55
spiraspira <zabornik5350@gmail.com>
2024-01-25 11:53:46 +0300

    Added Film entity

---
+++ FilmSearchPortal.DAL/Entities/Film.cs
@@ -0,0 +1,12 @@
+﻿namespace FilmSearchPortal.DAL.Entities;
+
+public class Film : Entity
+{
+	public string? Title { get; set; }
+
+	public string? Description { get; set; }
+
+	public int? ReleaseYear { get; set; }
+
+	public TimeSpan? Duration { get; set; }
+}
