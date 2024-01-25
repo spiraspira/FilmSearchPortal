9b4a2a62b832c0c6e41971e10b9a24dc183da4d1
spiraspira <zabornik5350@gmail.com>
2024-01-25 14:12:41 +0300

    Added ActorRepository

---
+++ FilmSearchPortal.DAL/Repositories/ActorRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace FilmSearchPortal.DAL.Repositories;
+
+public class ActorRepository(ApplicationDbContext context) : GenericRepository<Actor>(context), IActorRepository
+{
+	public async Task<IEnumerable<Actor>> GetActorsByName(string query)
+	{
+		return await Set
+			.Where(actor => actor.FirstName != null &&
+							actor.LastName != null &&
+							string.Concat(actor.FirstName, " ", actor.LastName).Contains(query))
+			.ToListAsync();
+	}
+
+	public override Task<Actor?> Get(Guid id)
+	{
+		return Set
+			.Include(film => film.FilmActors)
+			.ThenInclude(filmActor => filmActor.Film)
+			.FirstOrDefaultAsync(film => film.Id == id);
+	}
+}
