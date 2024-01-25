4cf723603d11d21d5a4659646ba09bba94577691
spiraspira <zabornik5350@gmail.com>
2024-01-25 14:29:50 +0300

    Added DataAccessRegister

---
+++ FilmSearchPortal.DAL/DI/DataAccessRegister.cs
@@ -0,0 +1,18 @@
+﻿namespace FilmSearchPortal.DAL.DI;
+
+public static class DataAccessRegister
+{
+	public static void AddDataAccess(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+		});
+
+		services.AddScoped<IActorRepository, ActorRepository>();
+
+		services.AddScoped<IFilmRepository, IFilmRepository>();
+
+		services.AddScoped<IReviewRepository, ReviewRepository>();
+	}
+}
