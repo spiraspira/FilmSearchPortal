0bcc8fde9d56a6838e55e5ed51f900db304cedeb
spiraspira <zabornik5350@gmail.com>
2024-01-25 11:55:38 +0300

    Added Actor Entity

---
+++ FilmSearchPortal.DAL/Entities/Actor.cs
@@ -0,0 +1,12 @@
+﻿namespace FilmSearchPortal.DAL.Entities;
+
+internal class Actor : Entity
+{
+	public string? FirstName { get; set; }
+
+	public string? LastName { get; set; }
+
+	public DateTime? DateOfBirth { get; set; }
+
+	public string? Biography { get; set; }
+}
