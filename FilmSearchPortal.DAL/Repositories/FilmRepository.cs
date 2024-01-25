af37b298dcefab75b0ef5256329c43ad1a80cae5
spiraspira <zabornik5350@gmail.com>
2024-01-25 14:13:17 +0300

    Fixed FilmRepository Get lamda parameter name

---
+++ FilmSearchPortal.DAL/Repositories/FilmRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace FilmSearchPortal.DAL.Repositories;
+
+public class FilmRepository(ApplicationDbContext context) : GenericRepository<Film>(context), IFilmRepository
+{
+	public async Task<IEnumerable<Film>> GetFilmsByTitle(string query)
+	{
+		return await Set
+			.Where(film => film.Title != null && film.Title.Contains(query))
+			.ToListAsync();
+	}
+
+	public override Task<Film?> Get(Guid id)
+	{
+		return Set
+			.Include(film => film.Reviews)
+			.Include(film => film.FilmActors)
+			.ThenInclude(filmActor => filmActor.Actor)
+			.FirstOrDefaultAsync(film => film.Id == id);
+	}
+}
