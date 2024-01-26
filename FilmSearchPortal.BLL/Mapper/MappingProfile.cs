40f269bb716f5f72721b5d53bed86f4c8c02e257
spiraspira <zabornik5350@gmail.com>
2024-01-26 13:51:00 +0300

    Added AutoMapper

---
+++ FilmSearchPortal.BLL/Mapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace FilmSearchPortal.BLL.Mapper;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<Actor, ActorModel>().ReverseMap();
+
+		CreateMap<Film, FilmModel>().ReverseMap();
+
+		CreateMap<FilmActor, FilmActorModel>().ReverseMap();
+
+		CreateMap<Review, ReviewModel>().ReverseMap();
+	}
+}
