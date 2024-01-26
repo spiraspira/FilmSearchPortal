522a90d45550e4a5451d7641c25cf0fa64adc080
spiraspira <zabornik5350@gmail.com>
2024-01-26 17:54:53 +0300

    Added ActorService

---
+++ FilmSearchPortal.BLL/Services/ActorService.cs
@@ -0,0 +1,21 @@
+﻿namespace FilmSearchPortal.BLL.Services;
+
+public class ActorService : GenericService<ActorModel, Actor>, IActorService
+{
+	private readonly IActorRepository _repository;
+
+	private readonly IMapper _mapper;
+
+	public ActorService(IActorRepository repository, IMapper mapper) : base(repository, mapper)
+	{
+		_repository = repository;
+		_mapper = mapper;
+	}
+
+	public async Task<IEnumerable<ActorModel>> GetActorsByName(string query)
+	{
+		var entities = await _repository.GetActorsByName(query);
+
+		return _mapper.Map<IEnumerable<ActorModel>>(entities);
+	}
+}
