0954799c05a255dda8caa060ebb300dd4f6c4790
spiraspira <zabornik5350@gmail.com>
2024-01-26 13:30:23 +0300

    Added ActorModel

---
+++ FilmSearchPortal.BLL/Models/ActorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FilmSearchPortal.BLL.Models;
+
+public class ActorModel
+{
+	public Guid Id { get; set; }
+
+	public string? FirstName { get; set; }
+
+	public string? LastName { get; set; }
+
+	public DateTime DateOfBirth { get; set; }
+
+	public string? Biography { get; set; }
+}
