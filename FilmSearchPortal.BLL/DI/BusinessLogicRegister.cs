738ee414f47d3f6c52e4f5383fc23e55043360e5
spiraspira <zabornik5350@gmail.com>
2024-01-26 18:14:17 +0300

    Added BLL DI

---
+++ FilmSearchPortal.BLL/DI/BusinessLogicRegister.cs
@@ -0,0 +1,18 @@
+﻿using FilmSearchPortal.BLL.Services;
+using FilmSearchPortal.DAL.DI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilmSearchPortal.BLL.DI;
+
+public static class BusinessLogicRegister
+{
+	public static void AddBusinessLogic(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddScoped<IActorService, ActorService>();
+
+		services.AddScoped<IFilmService, FilmService>();
+
+		services.AddDataAccess(config);
+	}
+}
