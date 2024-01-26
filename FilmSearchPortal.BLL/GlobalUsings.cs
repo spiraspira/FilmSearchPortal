5ee0b7dd6883a258f6536a40775434da97f3991e
spiraspira <zabornik5350@gmail.com>
2024-01-26 17:06:28 +0300

    Added GenericService

--- FilmSearchPortal.BLL/GlobalUsings.cs
+++ FilmSearchPortal.BLL/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using AutoMapper;
 global using FilmSearchPortal.BLL.Interfaces;
 global using FilmSearchPortal.BLL.Models;
 global using FilmSearchPortal.DAL.Entities;
+global using FilmSearchPortal.DAL.Interfaces;
