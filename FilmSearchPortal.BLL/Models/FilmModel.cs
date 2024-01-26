aaddf925021501e42241c8e4725d6f23599bdb27
spiraspira <zabornik5350@gmail.com>
2024-01-26 13:29:42 +0300

    Added FilmModel

---
+++ FilmSearchPortal.BLL/Models/FilmModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FilmSearchPortal.BLL.Models;
+
+public class FilmModel
+{
+	public Guid? Id { get; set; }
+
+	public string? Title { get; set; }
+
+	public string? Description { get; set; }
+
+	public int? ReleaseYear { get; set; }
+
+	public TimeSpan Duration { get; set; }
+}
