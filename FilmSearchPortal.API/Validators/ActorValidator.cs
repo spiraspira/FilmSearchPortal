d316af252a87c1781262b86cfdbb38c2785f1474
spiraspira <zabornik5350@gmail.com>
2024-01-27 14:11:04 +0300

    Added ActorValidator

---
+++ FilmSearchPortal.API/Validators/ActorValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace FilmSearchPortal.API.Validators;
+
+public class ActorValidator : AbstractValidator<ActorViewModel>
+{
+	public ActorValidator()
+	{
+		RuleFor(actor => actor.FirstName)
+			.NotNull()
+			.Length(2, 50)
+			.WithMessage("First name must be between {MinLength} and {MaxLength} characters.");
+
+		RuleFor(actor => actor.LastName)
+			.NotNull()
+			.Length(2, 50)
+			.WithMessage("Last name must be between {MinLength} and {MaxLength} characters.");
+
+		RuleFor(actor => actor.DateOfBirth)
+			.NotNull()
+			.LessThanOrEqualTo(DateTime.Now)
+			.WithMessage("Date of birth cannot be in the future.");
+	}
+}
