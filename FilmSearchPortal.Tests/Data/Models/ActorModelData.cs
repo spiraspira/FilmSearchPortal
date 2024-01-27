d135d725d11e30eb79ed5597bcc04aecf4dcfed0
spiraspira <zabornik5350@gmail.com>
2024-01-27 12:33:06 +0300

    Added ActorServiceTest

---
+++ FilmSearchPortal.Tests/Data/Models/ActorModelData.cs
@@ -0,0 +1,21 @@
+﻿namespace FilmSearchPortal.Tests.Data.Models;
+
+public static class ActorModelData
+{
+	public static ActorModel InitActorModel()
+	{
+		var actor = new ActorModel
+		{
+			Id = Guid.NewGuid(),
+			FirstName = "John",
+			LastName = "Doe"
+		};
+
+		return actor;
+	}
+
+	public static List<ActorModel> InitActorModelCollection()
+	{
+		return [InitActorModel()];
+	}
+}
---
+++ FilmSearchPortal.Tests/GlobalUsings.cs
@@ -0,0 +1,13 @@
+// Global using directives
+
+global using AutoMapper;
+global using FilmSearchPortal.BLL.Interfaces;
+global using FilmSearchPortal.BLL.Models;
+global using FilmSearchPortal.BLL.Services;
+global using FilmSearchPortal.DAL.Entities;
+global using FilmSearchPortal.DAL.Interfaces;
+global using FilmSearchPortal.Tests.Data.Entities;
+global using FilmSearchPortal.Tests.Data.Models;
+global using Moq;
+global using Shouldly;
+global using Xunit;
