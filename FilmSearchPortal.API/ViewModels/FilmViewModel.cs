25d3f644a6687c304ae0cc379427c0d1fa1829c1
spiraspira <zabornik5350@gmail.com>
2024-01-27 13:51:00 +0300

    Added FilmViewModel

---
+++ FilmSearchPortal.API/ViewModels/FilmViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FilmSearchPortal.API.ViewModels;
+
+public class FilmViewModel
+{
+	public Guid Id { get; set; }
+
+	public string? Title { get; set; }
+
+	public string? Description { get; set; }
+
+	public int? ReleaseYear { get; set; }
+
+	public TimeSpan Duration { get; set; }
+}
