3ff188e928cad8debe7542d867c2b73356714ab0
spiraspira <zabornik5350@gmail.com>
2024-01-27 13:50:17 +0300

    Added ActorViewModel

---
+++ FilmSearchPortal.API/ViewModels/ActorViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FilmSearchPortal.API.ViewModels;
+
+public class ActorViewModel
+{
+	public Guid Id { get; set; }
+
+	public string? FirstName { get; set; }
+
+	public string? LastName { get; set; }
+
+	public DateTime DateOfBirth { get; set; }
+
+	public string? Biography { get; set; }
+}
