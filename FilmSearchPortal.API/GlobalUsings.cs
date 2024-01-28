9628d37dc75077c355a215f5e5e7ac20344cb020
spiraspira <zabornik5350@gmail.com>
2024-01-28 18:54:33 +0300

    Added ReferenceLoopHandling

--- FilmSearchPortal.API/GlobalUsings.cs
+++ FilmSearchPortal.API/GlobalUsings.cs
@@ -1,11 +1,8 @@
 // Global using directives
 
-global using System.Net;
-global using System.Reflection;
 global using AutoMapper;
 global using FilmSearchPortal.API.Mapper;
 global using FilmSearchPortal.API.Middleware;
-global using FilmSearchPortal.API.Validators;
 global using FilmSearchPortal.API.ViewModels;
 global using FilmSearchPortal.BLL.DI;
 global using FilmSearchPortal.BLL.Interfaces;
@@ -15,3 +12,5 @@ global using Microsoft.AspNetCore.Mvc;
 global using Newtonsoft.Json;
 global using Serilog;
 global using Serilog.Events;
+global using System.Net;
+global using System.Reflection;
