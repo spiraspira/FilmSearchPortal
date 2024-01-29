516cd1e1c1a033897b33db933320642864bc0132
spiraspira <zabornik5350@gmail.com>
2024-01-29 11:35:20 +0300

    Added FilmActorController

---
+++ FilmSearchPortal.API/Controllers/FilmActorController.cs
@@ -0,0 +1,41 @@
+﻿namespace FilmSearchPortal.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilmActorController(
+	IMapper mapper,
+	IGenericService<FilmActorModel> filmActorService)
+	: ControllerBase
+{
+	[HttpGet("{id}")]
+	public async Task<FilmActorViewModel> Get(Guid id)
+	{
+		return mapper.Map<FilmActorViewModel>(await filmActorService.Get(id));
+	}
+
+	[HttpGet]
+	public async Task<IEnumerable<FilmActorViewModel>> GetAll()
+	{
+		return mapper.Map<IEnumerable<FilmActorViewModel>>(await filmActorService.GetAll());
+	}
+
+	[HttpPost]
+	public async Task<FilmActorViewModel> Create(FilmActorViewModel model)
+	{
+		return mapper.Map<FilmActorViewModel>(
+			await filmActorService.Create(mapper.Map<FilmActorModel>(model)));
+	}
+
+	[HttpPut]
+	public async Task<FilmActorViewModel> Update(FilmActorViewModel model)
+	{
+		return mapper.Map<FilmActorViewModel>(
+			await filmActorService.Update(mapper.Map<FilmActorModel>(model)));
+	}
+
+	[HttpDelete]
+	public async Task<FilmActorViewModel> Delete(Guid id)
+	{
+		return mapper.Map<FilmActorViewModel>(await filmActorService.Delete(id));
+	}
+}
