26ca4dd0f369a26a44591ee0a2933e0d75ce9d7d
spiraspira <zabornik5350@gmail.com>
2024-01-29 14:16:05 +0300

    Added FilmServiceTest

---
+++ FilmSearchPortal.Tests/Data/Entities/FilmEntityData.cs
@@ -0,0 +1,20 @@
+﻿namespace FilmSearchPortal.Tests.Data.Entities;
+
+public static class FilmEntityData
+{
+	public static Film InitFilmEntity()
+	{
+		var film = new Film
+		{
+			Id = Guid.NewGuid(),
+			Title = "Test"
+		};
+
+		return film;
+	}
+
+	public static List<Film> InitFilmEntityCollection()
+	{
+		return [InitFilmEntity()];
+	}
+}
